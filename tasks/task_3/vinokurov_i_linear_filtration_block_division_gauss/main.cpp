31111f039a34564d9e460a8f88d5d341159ffc41
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2023-12-23 21:50:58 +0300

    code style fix 2

--- tasks/task_3/vinokurov_i_linear_filtration_block_division_gauss/main.cpp
+++ tasks/task_3/vinokurov_i_linear_filtration_block_division_gauss/main.cpp
@@ -21,6 +21,74 @@ TEST(TESTS, CanUseFunctionTest) {
     ASSERT_NO_THROW(result = applyFilterMPI(image));
 }
 
+TEST(TESTS, CanUseFunctionTest2) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::vector<std::vector<int>> image = {
+        {100, 120, 130, 110, 120},
+        {110, 115, 120, 125, 105},
+        {90, 100, 110, 120, 130},
+        {95, 105, 115, 125, 135},
+        {120, 130, 140, 150, 110}
+    };
+
+    std::vector<std::vector<int>> result;
+
+    ASSERT_NO_THROW(result = applyFilterMPI(image));
+}
+
+TEST(TESTS, CanUseFunctionTest3) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::vector<std::vector<int>> image = {
+        {100, 120, 130, 110, 120},
+        {110, 115, 120, 125, 105},
+        {90, 100, 110, 120, 130},
+        {95, 105, 115, 125, 135},
+        {120, 130, 140, 150, 110}
+    };
+
+    std::vector<std::vector<int>> result;
+
+    ASSERT_NO_THROW(result = applyFilterMPI(image));
+}
+
+TEST(TESTS, CanUseFunctionTest4) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::vector<std::vector<int>> image = {
+        {100, 120, 130, 110, 120},
+        {110, 115, 120, 125, 105},
+        {90, 100, 110, 120, 130},
+        {95, 105, 115, 125, 135},
+        {120, 130, 140, 150, 110}
+    };
+
+    std::vector<std::vector<int>> result;
+
+    ASSERT_NO_THROW(result = applyFilterMPI(image));
+}
+
+TEST(TESTS, CanUseFunctionTest5) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::vector<std::vector<int>> image = {
+        {100, 120, 130, 110, 120},
+        {110, 115, 120, 125, 105},
+        {90, 100, 110, 120, 130},
+        {95, 105, 115, 125, 135},
+        {120, 130, 140, 150, 110}
+    };
+
+    std::vector<std::vector<int>> result;
+
+    ASSERT_NO_THROW(result = applyFilterMPI(image));
+}
+
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
   ::testing::InitGoogleTest(&argc, argv);
