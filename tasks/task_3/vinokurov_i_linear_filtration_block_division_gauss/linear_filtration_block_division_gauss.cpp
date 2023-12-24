e7336205b4c365675c8fef58648699178d0a8e2b
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2023-12-24 22:50:06 +0300

    a esli tak

--- tasks/task_3/vinokurov_i_linear_filtration_block_division_gauss/linear_filtration_block_division_gauss.cpp
+++ tasks/task_3/vinokurov_i_linear_filtration_block_division_gauss/linear_filtration_block_division_gauss.cpp
@@ -101,8 +101,8 @@ std::vector<std::vector<unsigned char>> applyFilterMPI(const std::vector<std::ve
     displs[size - 1] = (size - 1) * blockRows * cols;
 
     std::vector<unsigned char> all_pixels(rows * cols);
-    MPI_Gatherv(&flattenLocalOutput[0], flattenLocalOutput.size(), MPI_UNSIGNED_CHAR,
-        &all_pixels[rank * cols], sendcounts.data(), displs.data(), MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Allgather(&flattenLocalOutput[0], flattenLocalOutput.size(), MPI_UNSIGNED_CHAR,
+        &all_pixels[cols * blockStart], rows * cols, MPI_UNSIGNED_CHAR, MPI_COMM_WORLD);
 
     auto itr = all_pixels.begin();
     for (int i = 0; i < rows; ++i) {
