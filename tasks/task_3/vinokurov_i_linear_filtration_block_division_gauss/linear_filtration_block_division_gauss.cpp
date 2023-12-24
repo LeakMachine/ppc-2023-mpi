31ed6a7f1600e39f7376521f3bd40ebb5e5feb8a
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2023-12-24 19:27:57 +0300

    e

--- tasks/task_3/vinokurov_i_linear_filtration_block_division_gauss/linear_filtration_block_division_gauss.cpp
+++ tasks/task_3/vinokurov_i_linear_filtration_block_division_gauss/linear_filtration_block_division_gauss.cpp
@@ -88,10 +88,10 @@ std::vector<std::vector<unsigned char>> applyFilterMPI(const std::vector<std::ve
         return localOutput;
     }
 
-    std::vector<std::vector<unsigned char>> localOutputAll(size * (blockEnd - blockStart), 
+    std::vector<std::vector<unsigned char>> localOutputAll(size * (blockEnd - blockStart),
                                            std::vector<unsigned char>(cols));
     MPI_Allgather(&localOutput[0][0], (blockEnd - blockStart) * cols, MPI_UNSIGNED_CHAR,
-        &localOutputAll[rank * (blockEnd - blockStart)][0], 
+        &localOutputAll[rank * (blockEnd - blockStart)][0],
         (blockEnd - blockStart) * cols, MPI_UNSIGNED_CHAR, MPI_COMM_WORLD);
 
     std::vector<std::vector<unsigned char>> finalOutput(rows, std::vector<unsigned char>(cols));
